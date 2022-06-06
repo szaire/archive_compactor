34e8b89abe78425bede1531016a518cb10fd5d6b
Samuel Zaire <samuel.zairerocha@gmail.com>
2022-06-06 19:33:36 -0300

    added comments

--- src/arvore_binaria/ArvoreBinaria.java
+++ src/arvore_binaria/ArvoreBinaria.java
@@ -30,29 +30,32 @@ public class ArvoreBinaria
         }
     }
 
-    // TODO: implementar o dicionário
     public void subPRE_ORDER(NodeP refNode) {
         System.out.print(refNode.data + " ");
 
+        // irá acessar os nós esquerdos até encontrar um node sem filhos
         if (refNode.left != null) {
             code += "0";
             subPRE_ORDER(refNode.left);
         }
 
+        // irá acessar os nós direitos até encontrar um node sem filhos
         if (refNode.right != null) {
             code += "1";
             subPRE_ORDER(refNode.right);
         }
 
+        // quando encontrar o node sem filhos, verificará se é diferente de nulo
         if (refNode.data != '\0') {
+            // se for, adicionará este na fila do dicionário e guardará seu código binário
             this.dictionary.enqueue(refNode.data, code);
         }
+        // irá verificar se o código binário é maior que 0 (para evitar null pointer exception)
         if (code.length() > 0) {
+            // irá tirar o último digito da string code para que se possa colocar outros futuramente
             this.code = code.substring(0, code.length()-1);
         }
     }
-
-
 
     // Utils:
     public boolean isEmpty() {
